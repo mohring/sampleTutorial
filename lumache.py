92fbc478a74b61914d1633423882b485fc1cd0c5
mohring <jan.mohring@itwm.fraunhofer.de>
2022-05-24 15:44:00 +0200

    Update lumache.py

--- lumache.py
+++ lumache.py
@@ -14,6 +14,7 @@ def get_random_ingredients(kind=None):
     """
     Return a list of random ingredients as strings.
     And a random formula.
+    
     :param kind: Optional "kind" of ingredients.
     :type kind: list[str] or None
     :raise lumache.InvalidKindError: If the kind is invalid.
